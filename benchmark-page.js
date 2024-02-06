82759c6292a3e2e3ddb3c0f321030743c473731f
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-06 17:33:03 +0100

    Prova creazione Quiz

--- benchmark-page.js
+++ benchmark-page.js
@@ -82,7 +82,29 @@ const questions = [
   },
 ];
 
-const startButton = document.getElementById("start-btn");
+const contenitoreInput = document.getElementsByClassName("contenitore-risposte");
+
+console.log(contenitoreInput);
+
+const questionSelezionate = () => {
+  for (let q = 0; q < questions.length; q++) {
+    const question = questions[q].question;
+    let questionRandom = function () {
+      const domandaRandom = Math.floor(Math.random() * question);
+      console.dir(questionRandom);
+    };
+  }
+};
+questionSelezionate();
+
+const arrAnswers = () => {
+  for (let i = 0; i < questions.length; i++) {
+    const correctAnswer = questions[i].correct_answer;
+    const incorrectAnswers = questions[i].incorrect_answers;
+  }
+};
+arrAnswers();
+/*const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 
 const questionContainer = document.getElementsByClassName("question-container");
@@ -134,34 +156,42 @@ function showQuestion() {
   function nextButton() {
     const nextBtn = document.getElementById("next-btn");
   }
+*/
+/*
+for (let i = 0; i < questions.length; i++) {
+  function domandeCasuali(arr) {
+    const arrDomande = document.getElementsByClassName("question-container")[0];
+    const domande = questions[i].question;
 
-  // for (let i = 0; i < questions.length; i++) {
-  //   // console.log(questions[i].correct_answer);
-  //   const input = document.getElementsByClassName("option");
-  //   const domande = questions[i].question;
-  //   const correctAns = questions[i].correct_answer;
-  //   const incorrectAns = questions[i].incorrect_answers;
-  //   for (let j = 0; j < questions.length; j++) {
-  //     const element = questions[j];
-  //   }
+    console.log(arrDomande);
 
-  // }
-  // const answers = document.getElementsByClassName("option");
-  // answers.innerHTML = currentQuestion.incorrect_answers;
-}
+    for (let q = 0; q < arrDomande.length; q++) {
+      let domandaRandom = Math.floor(Math.random() * arr.length);
+    }
+  }
+  const correctAns = questions[i].correct_answer;
+  const incorrectAns = questions[i].incorrect_answers;
+  for (let j = 0; j < questions.length; j++) {
+    const element = questions[j];
+  }
+*/
+// }
+// const answers = document.getElementsByClassName("option");
+// answers.innerHTML = currentQuestion.incorrect_answers;
+//}
 
-window.onload = function () {
-  showQuestion();
-  // TIPS:
-  // SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
-  // Per ogni domanda, crea un container e incorporale tutte all'interno.
-  // Crea poi dei radio button
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
-  // con le risposte corrette e incorrette come opzioni
-  // (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale)
-  //
-  // SE MOSTRI UNA DOMANDA ALLA VOLTA:
-  // Mostra la prima domanda con il testo e i radio button.
-  // Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
-  // salvando le risposte dell'utente in una variabile
-};
+//window.onload = function () {
+// showQuestion();
+// TIPS:
+// SE MOSTRI TUTTE LE RISPOSTE ASSIEME IN FORMATO LISTA:
+// Per ogni domanda, crea un container e incorporale tutte all'interno.
+// Crea poi dei radio button
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio
+// con le risposte corrette e incorrette come opzioni
+// (dovrai probabilmente cercare su un motore di ricerca come ottenere un valore da un radio button in JS per ottenere il punteggio finale)
+//
+// SE MOSTRI UNA DOMANDA ALLA VOLTA:
+// Mostra la prima domanda con il testo e i radio button.
+// Quando l'utente seleziona una risposta, passa alla domanda successiva dell'array e sostituisci quella precedentemente visualizzata con quella corrente,
+// salvando le risposte dell'utente in una variabile
+//};
