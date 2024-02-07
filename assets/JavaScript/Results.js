647165041c187fda7489cbd5512b97b544dc9df3
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-07 22:08:32 +0100

    Struttura creata CSS and html

--- assets/JavaScript/Results.js
+++ assets/JavaScript/Results.js
@@ -1,1 +1,17 @@
 //RESULTES
+const semiCircleQuiz = document.querySelectorAll(".semicircle-quiz");
+
+const risposteCorette = document.querySelector(".risposte-corette");
+const risposteSbagliate = document.querySelector(".risposte-sbagliate");
+
+//input
+//AGGIUNGERE ALLE CONST risposteCorette e risposteSbagliate il risultato del test//
+
+const rightRisposte = 0;
+const wrongRisposte = 0;
+
+const giusteRisposte = rightRisposte; // +  quelle che mi arrivano dal test;
+const sbagliateRisposte = wrongRisposte; // + quelle che mi arrivano dal test;
+const totRisposte = giusteRisposte + sbagliateRisposte;
+
+//function countRightAndWrong() {}
