e7582e0e7e3f9be9d8c2d5e813e0b3b8322779a0
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-08 11:17:38 +0100

    commit

--- assets/JavaScript/Results.js
+++ assets/JavaScript/Results.js
@@ -1,3 +1,133 @@
+let questionNumber = 0;
+let score = 0;
+const questionCounter = document.getElementById("number-of-question");
+
+const quizContainer = document.getElementById("quiz-container");
+const questionElement = document.getElementById("question");
+const optionsContainer = document.getElementById("options");
+
+function updateQuestionCounter(hasEnded = false) {
+  questionCounter.innerHTML = !hasEnded ? "QUESTION " + (questionNumber + 1) + "/" + questions.length : "";
+}
+
+function showQuestion() {
+  const currentQuestion = questions[questionNumber];
+  questionElement.innerText = currentQuestion.question;
+  optionsContainer.innerHTML = "";
+
+  if (currentQuestion.type === "multiple") {
+    const options = currentQuestion.incorrect_answers.concat(currentQuestion.correct_answer);
+    options.sort(() => Math.random() - 0.5);
+
+    options.forEach((option) => {
+      const radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "option";
+      radio.value = option;
+      radio.classList.add("option-input");
+
+      const label = document.createElement("label");
+      label.innerText = option;
+      label.classList.add("styled-label");
+
+      const optionsDiv = document.createElement("div");
+      optionsDiv.classList.add("options-container");
+
+      optionsDiv.appendChild(radio);
+      optionsDiv.appendChild(label);
+
+      optionsContainer.appendChild(optionsDiv);
+
+      label.addEventListener("click", function () {
+        radio.checked = true;
+      });
+      label.addEventListener("click", checkAnswer);
+    });
+  } else if (currentQuestion.type === "boolean") {
+    const optionTrue = document.createElement("input");
+    optionTrue.type = "radio";
+    optionTrue.name = "option";
+    optionTrue.value = "True";
+    optionTrue.classList.add("option-input");
+
+    const trueLabel = document.createElement("label");
+    trueLabel.innerText = "True";
+    trueLabel.classList.add("styled-label");
+
+    const trueDiv = document.createElement("div");
+    trueDiv.classList.add("options-container");
+    trueDiv.appendChild(optionTrue);
+    trueDiv.appendChild(trueLabel);
+
+    const falseOption = document.createElement("input");
+    falseOption.type = "radio";
+    falseOption.name = "option";
+    falseOption.value = "False";
+    falseOption.classList.add("option-input");
+
+    const falseLabel = document.createElement("label");
+    falseLabel.innerText = "False";
+    falseLabel.classList.add("styled-label");
+
+    const falseDiv = document.createElement("div");
+    falseDiv.classList.add("options-container");
+
+    falseDiv.appendChild(falseOption);
+    falseDiv.appendChild(falseLabel);
+
+    optionsContainer.appendChild(trueDiv);
+    optionsContainer.appendChild(falseDiv);
+
+    trueDiv.addEventListener("click", function () {
+      optionTrue.checked = true;
+    });
+    trueDiv.addEventListener("click", checkAnswer);
+
+    falseDiv.addEventListener("click", function () {
+      falseOption.checked = true;
+    });
+    falseDiv.addEventListener("click", checkAnswer);
+  }
+}
+
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="option"]:checked');
+  if (!selectedOption) return;
+
+  const answer = selectedOption.value;
+  const currentQuestion = questions[questionNumber];
+
+  if (answer === currentQuestion.correct_answer) {
+    score++;
+  }
+
+  questionNumber++;
+
+  if (questionNumber < questions.length) {
+    showQuestion();
+    updateQuestionCounter();
+  } else {
+    showScore();
+    updateQuestionCounter(true);
+  }
+}
+
+function showScore() {
+  quizContainer.innerHTML = `<h2>Your Score: ${score}/${questions.length}</h2>`;
+}
+
+window.onload = function () {
+  showQuestion();
+  updateQuestionCounter();
+};
+
+
+
+
+/////////////////////////////////////////////////////////
+//////////////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////
 //RESULTES
 const semiCircleQuiz = document.querySelectorAll(".semicircle-quiz");
 
@@ -11,7 +141,7 @@ const rightRisposte = 0;
 const wrongRisposte = 0;
 
 const giusteRisposte = rightRisposte + score;
-const sbagliateRisposte = wrongRisposte + (${question.leght} - {score});
+const sbagliateRisposte = wrongRisposte + (${questions.length} - {score});
 const totRisposte = giusteRisposte + sbagliateRisposte;
 
 
@@ -23,6 +153,13 @@ function countRightAndWrong() {
 
 
 // funzione per trasformare il risultato in percentuale
+const 
+
+
+
+
+
+
 
 // funzione per generare commento centrale
 
