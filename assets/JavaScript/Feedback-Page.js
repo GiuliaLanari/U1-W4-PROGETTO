710dd18f5e404f3bbfe114e232ba089da1efba13
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-02-08 14:10:43 +0100

    scheletro stelline

--- assets/JavaScript/Feedback-Page.js
+++ assets/JavaScript/Feedback-Page.js
@@ -1,1 +1,38 @@
 //FEEDBACK PAGE
+const ratingStars = [...document.getElementsByClassName("star")];
+
+function executeRating(stars) {
+  let selectedStars = [];
+
+  stars.forEach((star, i) => {
+    star.onclick = () => {
+      if (star.classList.contains("active")) {
+        star.classList.remove("active");
+      } else {
+        star.classList.add("active");
+      }
+    };
+
+    // selezionare le stelle
+    star.addEventListener("mouseover", () => {
+      selectedStars.forEach((selectedIndex) => {
+        stars[selectedIndex].classList.add("active");
+      });
+
+      for (let j = 0; j <= i; j++) {
+        stars[j].classList.add("active");
+      }
+    });
+
+    //mouseleave per ripristinare il colore originale
+    star.addEventListener("mouseleave", () => {
+      stars.forEach((star, index) => {
+        if (!selectedStars.includes(index)) {
+          star.classList.remove("active");
+        }
+      });
+    });
+  });
+}
+
+executeRating(ratingStars);
