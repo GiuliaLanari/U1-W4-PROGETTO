f985fd2e3831b5a0689a07e76d26b2152657796c
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-08 12:11:46 +0100

    inizio

--- assets/JavaScript/Results.js
+++ assets/JavaScript/Results.js
@@ -112,18 +112,6 @@ function checkAnswer() {
   }
 }
 
-function showScore() {
-  quizContainer.innerHTML = `<h2>Your Score: ${score}/${questions.length}</h2>`;
-}
-
-window.onload = function () {
-  showQuestion();
-  updateQuestionCounter();
-};
-
-
-
-
 /////////////////////////////////////////////////////////
 //////////////////////////////////////////////
 ///////////////////////////////////////
@@ -137,23 +125,6 @@ const risposteSbagliate = document.querySelector(".risposte-sbagliate");
 //input
 
 
-const rightRisposte = 0;
-const wrongRisposte = 0;
-
-const giusteRisposte = rightRisposte + score;
-const sbagliateRisposte = wrongRisposte + (${questions.length} - {score});
-const totRisposte = giusteRisposte + sbagliateRisposte;
-
-
-function countRightAndWrong() { 
-
-//funzione che si collega al grafico
-
-
-
-
-// funzione per trasformare il risultato in percentuale
-const 
 
 
 
@@ -161,43 +132,79 @@ const
 
 
 
-// funzione per generare commento centrale
 
-const commentoRisultato = document.getElementsByClassName("commento-risultato")
-const feedbackRisultato = () => {
 
-if (giusteRisposte> sbagliateRisposte) {
-    const paragrafo1 = document.createElement("p")
-    paragrafo1.classList.add("paragrafo1")
-    paragrafo1.innerText= "Congratulations!"
-    commentoRisultato.appendChild(paragrafo1)
 
-    const paragrafo2 = document.createElement("p")
-    paragrafo2.classList.add("paragrafo2")
-    paragrafo2.innerText= "You passed the exam."
-    commentoRisultato.appendChild(paragrafo2)
 
-    const paragrafo3 = document.createElement("p")
-    paragrafo3.classList.add("paragrafo3")
-    paragrafo3.innerText= "We'll send you the certificate in a few minutes. Check your email (including promotions/ spam folder) "
-    commentoRisultato.appendChild(paragrafo3)
 
-} else {
-    const paragrafo4 = document.createElement("p")
-    paragrafo4.classList.add("paragrafo4")
-    paragrafo4.innerText= "Exam failed!"
-    commentoRisultato.appendChild(paragrafo4)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function countRightAndWrong() {
+  //funzione che si collega al grafico
+
+  // funzione per trasformare il risultato in percentuale
+
+  // funzione per generare commento centrale
+
+  const commentoRisultato = document.getElementsByClassName("commento-risultato");
+
+  const feedbackRisultato = () => {
+    if (giusteRisposte > sbagliateRisposte) {
+      const paragrafo1 = document.createElement("p");
+      paragrafo1.classList.add("paragrafo1");
+      paragrafo1.innerText = "Congratulations!";
+      commentoRisultato.appendChild(paragrafo1);
+
+      const paragrafo2 = document.createElement("p");
+      paragrafo2.classList.add("paragrafo2");
+      paragrafo2.innerText = "You passed the exam.";
+      commentoRisultato.appendChild(paragrafo2);
+
+      const paragrafo3 = document.createElement("p");
+      paragrafo3.classList.add("paragrafo3");
+      paragrafo3.innerText =
+        "We'll send you the certificate in a few minutes. Check your email (including promotions/ spam folder) ";
+      commentoRisultato.appendChild(paragrafo3);
+    } else {
+      const paragrafo4 = document.createElement("p");
+      paragrafo4.classList.add("paragrafo4");
+      paragrafo4.innerText = "Exam failed!";
+      commentoRisultato.appendChild(paragrafo4);
+    }
+  };
 }
 
-
-
-
-
-}
-
-
-
-
-
-}
+window.onload = function () {
+  showQuestion();
+  updateQuestionCounter();
+};
