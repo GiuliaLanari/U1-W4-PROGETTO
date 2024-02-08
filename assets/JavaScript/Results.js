bd35e0f399e2022457fc695cd2d3323ac87642d9
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-08 10:56:18 +0100

    commento JavaScript

--- assets/JavaScript/Results.js
+++ assets/JavaScript/Results.js
@@ -5,13 +5,62 @@ const risposteCorette = document.querySelector(".risposte-corette");
 const risposteSbagliate = document.querySelector(".risposte-sbagliate");
 
 //input
-//AGGIUNGERE ALLE CONST risposteCorette e risposteSbagliate il risultato del test//
+
 
 const rightRisposte = 0;
 const wrongRisposte = 0;
 
-const giusteRisposte = rightRisposte; // +  quelle che mi arrivano dal test;
-const sbagliateRisposte = wrongRisposte; // + quelle che mi arrivano dal test;
+const giusteRisposte = rightRisposte + score;
+const sbagliateRisposte = wrongRisposte + (${question.leght} - {score});
 const totRisposte = giusteRisposte + sbagliateRisposte;
 
-//function countRightAndWrong() {}
+
+function countRightAndWrong() { 
+
+//funzione che si collega al grafico
+
+
+
+
+// funzione per trasformare il risultato in percentuale
+
+// funzione per generare commento centrale
+
+const commentoRisultato = document.getElementsByClassName("commento-risultato")
+const feedbackRisultato = () => {
+
+if (giusteRisposte> sbagliateRisposte) {
+    const paragrafo1 = document.createElement("p")
+    paragrafo1.classList.add("paragrafo1")
+    paragrafo1.innerText= "Congratulations!"
+    commentoRisultato.appendChild(paragrafo1)
+
+    const paragrafo2 = document.createElement("p")
+    paragrafo2.classList.add("paragrafo2")
+    paragrafo2.innerText= "You passed the exam."
+    commentoRisultato.appendChild(paragrafo2)
+
+    const paragrafo3 = document.createElement("p")
+    paragrafo3.classList.add("paragrafo3")
+    paragrafo3.innerText= "We'll send you the certificate in a few minutes. Check your email (including promotions/ spam folder) "
+    commentoRisultato.appendChild(paragrafo3)
+
+} else {
+    const paragrafo4 = document.createElement("p")
+    paragrafo4.classList.add("paragrafo4")
+    paragrafo4.innerText= "Exam failed!"
+    commentoRisultato.appendChild(paragrafo4)
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+}
