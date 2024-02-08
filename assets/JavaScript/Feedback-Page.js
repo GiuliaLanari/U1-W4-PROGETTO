182d15cff2295ff3f7cbd6e5ff381378be50754a
Mia <155841134+miaansaloni@users.noreply.github.com>
2024-02-08 15:00:02 +0100

    stelline funzionanti

--- assets/JavaScript/Feedback-Page.js
+++ assets/JavaScript/Feedback-Page.js
@@ -1,38 +1,52 @@
 //FEEDBACK PAGE
-const ratingStars = [...document.getElementsByClassName("star")];
+document.addEventListener("DOMContentLoaded", function () {
+  const stars = document.querySelectorAll(".star");
 
-function executeRating(stars) {
-  let selectedStars = [];
+  let ratedStars = 0;
 
-  stars.forEach((star, i) => {
-    star.onclick = () => {
-      if (star.classList.contains("active")) {
-        star.classList.remove("active");
-      } else {
-        star.classList.add("active");
-      }
-    };
-
-    // selezionare le stelle
-    star.addEventListener("mouseover", () => {
-      selectedStars.forEach((selectedIndex) => {
-        stars[selectedIndex].classList.add("active");
-      });
-
-      for (let j = 0; j <= i; j++) {
-        stars[j].classList.add("active");
+  stars.forEach((star, index) => {
+    star.addEventListener("mouseover", function () {
+      // passaggio del mouse su ogni stella
+      for (let i = 0; i <= index; i++) {
+        // Illumina la stella corrente e quelle precedenti
+        stars[i].classList.add("active");
       }
     });
 
-    //mouseleave per ripristinare il colore originale
-    star.addEventListener("mouseleave", () => {
-      stars.forEach((star, index) => {
-        if (!selectedStars.includes(index)) {
-          star.classList.remove("active");
+    star.addEventListener("mouseout", function () {
+      // Rimuove l'illuminazione quando il mouse esce dalla stella
+      if (ratedStars === 0) {
+        for (let i = 0; i < stars.length; i++) {
+          stars[i].classList.remove("active");
         }
-      });
+      } else {
+        for (let i = 0; i < ratedStars; i++) {
+          stars[i].classList.add("active");
+        }
+      }
+    });
+
+    star.addEventListener("click", function () {
+      //event listener per il click su ogni stella
+      if (index === ratedStars - 1) {
+        // Controlla se la stella cliccata è già stata valutata
+        ratedStars = 0; // Se la stella cliccata è già valutata, annulla la valutazione
+        // Rimuovi l'illuminazione da tutte le stelle
+        stars.forEach((star) => {
+          star.classList.remove("active");
+        });
+      } else {
+        ratedStars = index + 1;
+        stars.forEach((star, i) => {
+          if (i <= index) {
+            star.classList.add("active"); // Imposta il nuovo rating
+          } else {
+            star.classList.remove("active");
+          }
+        });
+      }
+
+      console.log("Rated Stars:", ratedStars); // Salva il rating
     });
   });
-}
-
-executeRating(ratingStars);
+});
